8fff3664e96c069bc981bd229e36c21975ec67e8
rextao <87639243@qq.com>
2019-08-21 23:16:07 +0800

    create vue-source 2.6.10

---
+++ 4.框架相关/2.Vue/3.源码解析/vue/src/core/index.js
@@ -0,0 +1,28 @@
+// vue class的创建，以及prototype的挂载
+import Vue from './instance/index'
+import { initGlobalAPI } from './global-api/index'
+import { isServerRendering } from 'core/util/env'
+import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
+
+// 为vue挂载全局api
+initGlobalAPI(Vue)
+
+Object.defineProperty(Vue.prototype, '$isServer', {
+  get: isServerRendering
+})
+
+Object.defineProperty(Vue.prototype, '$ssrContext', {
+  get () {
+    /* istanbul ignore next */
+    return this.$vnode && this.$vnode.ssrContext
+  }
+})
+
+// expose FunctionalRenderContext for ssr runtime helper installation
+Object.defineProperty(Vue, 'FunctionalRenderContext', {
+  value: FunctionalRenderContext
+})
+
+Vue.version = '__VERSION__'
+
+export default Vue

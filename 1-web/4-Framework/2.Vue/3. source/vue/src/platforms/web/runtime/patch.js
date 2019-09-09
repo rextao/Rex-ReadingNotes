e62177b6b30b43ad898e413b83217e9f475d1db1
wangtao05 <wangtao05@kuaishou.com>
2019-09-09 09:07:24 +0800

    vue 源码解析

--- 1-web/4-Framework/2.Vue/3. source/vue/src/platforms/web/runtime/patch.js
+++ 1-web/4-Framework/2.Vue/3. source/vue/src/platforms/web/runtime/patch.js
@@ -3,9 +3,12 @@
 import * as nodeOps from 'web/runtime/node-ops'
 import { createPatchFunction } from 'core/vdom/patch'
 import baseModules from 'core/vdom/modules/index'
+// Todo 定义很多属性等钩子函数，创建dom还会有属性，style，利用platformModules里面的钩子进行处理
+// 之后会介绍
 import platformModules from 'web/runtime/modules/index'
 
 // 指令模块（baseModules）要在所有内置模块（platformModules）之后运行
 const modules = platformModules.concat(baseModules)
-
+// im： 高阶函数，利用柯里化解决不同平台差异，避免在函数中使用if-else进行判断，可以第一次统一解决不同平台差异问题
+// createPatchFunction是一个闭包，实际patch会执行createPatchFunction返回的函数
 export const patch: Function = createPatchFunction({ nodeOps, modules })

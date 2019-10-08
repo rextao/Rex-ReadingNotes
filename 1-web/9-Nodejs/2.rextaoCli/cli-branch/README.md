f6d55a3fb1203e40dca031bb6a84928e24f1cf37
rextao <87639243@qq.com>
2019-10-08 14:18:32 +0800

    修改config.js引入方式，增加ignore

--- 1-web/9-Nodejs/2.rextaoCli/cli-branch/README.md
+++ 1-web/9-Nodejs/2.rextaoCli/cli-branch/README.md
@@ -1,15 +1,14 @@
-# 概述
-1. 工作中用于删除gitlab已merged到develop分支的命令行软件
-
-
-# 学习
-## node操作git
+# 注意
+1. bin下需要创建一个config.js,并在index.js中引入`const config = require('./config');`
+# node操作git
 1. simple-git使用：支持promise，简化git的某些操作
-2. git 获取远程已合并分支
+1. git 获取远程已合并分支
 
-## 如何搞一个简单命令行
+# 其他
+1. 用自调用函数直接运行init函数
+# 如何搞一个简单命令行
 1. npm init 生成package.json 
-2. 添加bin
+1. 添加bin
     ```json
     {
       "bin": {
@@ -17,13 +16,10 @@
       }
     }
     ```
-3. 用 npm link 命令将这个简易脚本注册成系统命令行工具
+1. 用 npm link 命令将这个简易脚本注册成系统命令行工具
 
-## 美化输入输出控制
+# 美化输入输出控制
 1. ora主要是等待标
-1. prompts，例子丰富,使用方便，支持promise
+1. prompts，例子丰富,使用方便
 1. enquirer，比prompts功能更强大，可扩展、可增加插件
 1. chalk，美化输出
-
-## 其他
-1. 用自调用函数直接运行init函数
--- 1-web/9-Nodejs/2.rextaoCli/cli-branch/bin/index.js
+++ 1-web/9-Nodejs/2.rextaoCli/cli-branch/bin/index.js
@@ -1,15 +1,10 @@
 #! /usr/bin/env node
-
 const prompts = require('prompts');
 const chalk = require('chalk');
 const open = require('open');
+const config = require('./config');
 
-
-const config = {
-    workSpace: '/Users/rextao',
-    branchAlias: ['wt-', 'wangtao-']
-};
-let searchUrl = ''; // 类似于：xxxxxxxadmin/-/branches/all?utf8=✓&search=wt-  地址
+let searchUrl = '' ; // 类似于：xxxxxxxadmin/-/branches/all?utf8=✓&search=wt-  地址
 let isOpenUrl = false;
 const simpleGit = require('simple-git/promise')(config.workSpace);
 
@@ -21,77 +16,74 @@ const branchCache = {
 };
 
 
-(async function init() {
+(async function init(){
     await getLocalBranches(); // 要先获取本地分支，再切换到develop，最后再切换回当前分支
-    await getSearchUrl(); // 可能是不同项目，search地址不同
-    await developPull(); // 不拉最新的develop代码，不能准确获取已合并分支
+    await getSearchUrl();    // 可能是不同项目，search地址不同
+    await developPull();     // 不拉最新的develop代码，不能准确获取已合并分支
     try {
-        await getMergedBranches(); // 获取merged分支
-        await confirmModeSelect(); // 选择删除模式
-        await confirmOpenUrl(); // 选择是否打开网页，确定分支已经合并，避免误删
-        const isDelete = await confirmDeleteBranches(); // 确定删除远程分支
+        await getMergedBranches();  // 获取merged分支
+        await confirmModeSelect();  // 选择删除模式
+        await confirmOpenUrl();     // 选择是否打开网页，确定分支已经合并，避免误删
+        const isDelete = await confirmDeleteBranches();  // 确定删除远程分支
         // 如确定要删除
-        if (isDelete) {
-            await deleteRemoteTracking(); // 确定删除本地分支
+        if(isDelete) {
+            await deleteRemoteTracking();  // 确定删除本地分支
         }
-    } finally {
+    }finally {
         await co(branchCache.current);
     }
 })()
+
+
+
 // 删除远程分支tracking
-async function deleteRemoteTracking() {
-    await branchCache.readyDelete.map(async (item) => {
+async function deleteRemoteTracking(){
+    const readyDelete = branchCache.readyDelete;
+    for (let i = 0; i < readyDelete; i ++) {
+        const item = readyDelete[i];
         try {
-            await simpleGit.raw(['branch', '-r', '-d', `origin/${item}`]);
+            await simpleGit.raw(['branch','-r','-d',`origin/${item}`]);
             await simpleGit.push('origin', `:${item}`);
             console.log(chalk.green(`${item}远程分支删除成功`));
-            await confirmDeleteLocalBranch();
+            await confirmDeleteLocalBranch(item);
         } catch (e) {
             console.log(e);
         }
-    });
+    }
 
 }
 // 获取branch查询地址，查看当前分支是否已经合并到develop，避免删错
-async function getSearchUrl() {
+async function getSearchUrl(){
     const remotes = await simpleGit.getRemotes(true);
-    const {
-        refs
-    } = remotes.filter(item => item.name === 'origin')[0];
-    searchUrl = refs.fetch.match(/git@(.*)\.git/)[1].replace(':', '/');
+    const { refs } = remotes.filter(item => item.name === 'origin')[0];
+    searchUrl =  refs.fetch.match(/git@(.*)\.git/)[1].replace(':','/');
 }
 // 拉取develop分支最新内容
-async function developPull() {
+async function developPull(){
     await co(); // 切换到develop分支
-    const a = await simpleGit.pull();
+    const a =await simpleGit.pull();
 }
 // 是否删除本地分支
-async function confirmDeleteLocalBranch() {
-    const {
-        value
-    } = await prompts({
+async function confirmDeleteLocalBranch(item){
+    const { value } = await prompts({
         type: 'toggle',
         name: 'value',
         message: '是否删除本地分支?',
         active: 'Yes',
         inactive: 'No'
     });
-    if (value) {
-        branchCache.readyDelete.map(async (item) => {
-            try {
-                await simpleGit.deleteLocalBranch(`${item}`);
-                console.log(chalk.green(`本地分支删除成功`));
-            } catch (e) {
-                console.log(e);
-            }
-        });
+    if(value){
+        try {
+            await simpleGit.deleteLocalBranch(item);
+            console.log(chalk.green(`本地分支删除成功`));
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
 // 删除模式选择
 async function confirmModeSelect() {
-    const {
-        value
-    } = await prompts({
+    const { value } = await prompts({
         type: 'toggle',
         name: 'value',
         message: '选择删除分支模式?',
@@ -101,10 +93,8 @@ async function confirmModeSelect() {
     // 选择全部删除时
     if (value) {
         branchCache.readyDelete = branchCache.merged;
-    } else { //选择删除
-        const {
-            value
-        } = await prompts({
+    } else {//选择删除
+        const { value } = await prompts({
             type: 'multiselect',
             name: 'value',
             message: '选择要删除的分支！',
@@ -113,10 +103,8 @@ async function confirmModeSelect() {
         branchCache.readyDelete = value;
     }
 }
-async function confirmOpenUrl() {
-    const {
-        value
-    } = await prompts({
+async function confirmOpenUrl(){
+    const { value } = await prompts({
         type: 'toggle',
         name: 'value',
         message: '选择是否要自动打开search网页?',
@@ -131,14 +119,12 @@ async function confirmDeleteBranches() {
     const msgArr = branchCache.readyDelete.map((item) => {
         const url = `http://${searchUrl}/-/branches/all?search=${item}`;
         // 避免打开网页过多
-        if (isOpenUrl && branchCache.readyDelete.length <= 10) {
+        if(isOpenUrl && branchCache.readyDelete.length <= 10){
             open(url);
         }
         return `${chalk.magenta(item)}: ${url}`;
     });
-    const {
-        value
-    } = await prompts({
+    const { value } = await prompts({
         type: 'toggle',
         name: 'value',
         message: `是否要删除这些分支?\n${msgArr.join('\n')}\n`,
@@ -162,7 +148,7 @@ async function co(branchName = 'develop') {
 
 // 根据branchAlias，获取远程已合并分支
 async function getMergedBranches() {
-    const branches = await simpleGit.raw(['branch', '-r', '--merged']);
+    const branches = await simpleGit.raw(['branch','-r','--merged']);
     // 远程分支是以origin/开头,
     const reg = new RegExp(`(?<=origin\\/).*(${config.branchAlias.join('|')}).*`, 'g');
     branchCache.merged = branches.match(reg);
